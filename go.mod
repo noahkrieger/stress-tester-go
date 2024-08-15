42033944e2d2c70187615bd10b37c45951e36e99
Noah Krieger <noahkrieger@gmail.com>
2024-08-14 20:02:56 -0400

    Tweaked metrics collection.

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/noahkrieger/stress-tester-go
 
 go 1.22.5
 
-require github.com/prometheus/client_golang v1.19.1
+require (
+	github.com/prometheus/client_golang v1.19.1
+	golang.org/x/crypto v0.26.0
+)
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -10,6 +13,6 @@ require (
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
@@ -8,20 +10,27 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
+
+	"golang.org/x/crypto/ssh"
 )
 
 var totalRunRequests = prometheus.NewCounter(
 	prometheus.CounterOpts{
-		Name: "http_requests_total",
-		Help: "The total number of run HTTP requests.",
+		Name:      "http_requests_total",
+		Help:      "The total number of run HTTP requests.",
+		Namespace: "load_test",
+		Subsystem: "stress_tester_go",
 	},
 )
 
 var runDuration = prometheus.NewGauge(
 	prometheus.GaugeOpts{
-		Name: "run_duration_seconds",
-		Help: "The duration of the run.",
+		Name:      "run_duration_seconds",
+		Help:      "The duration of the run.",
+		Namespace: "load_test",
+		Subsystem: "stress_tester_go",
 	},
 )
 
@@ -37,11 +46,11 @@ func factorial(n uint64) uint64 {
 }
 
 func euler(iterations int) {
-
+	// 500_000 iterations takes around 500ms
 	for i := 0; i < iterations; i++ {
 		var n uint64
 		var e, term float64
-		for i := int(0); i < 65; i++ {
+		for j := int(0); j < 65; j++ {
 			term = float64(1) / float64(factorial(n))
 			e += term
 			n++
@@ -49,13 +58,47 @@ func euler(iterations int) {
 	}
 }
 
+func keygen(iterations int) error {
+	for i := 0; i < iterations; i++ {
+		privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+		if err != nil {
+			return err
+		}
+
+		pub, err := ssh.NewPublicKey(&privateKey.PublicKey)
+		if err != nil {
+			return err
+		}
+
+		_ = ssh.FingerprintSHA256(pub)
+	}
+	return nil
+}
+
 func run(w http.ResponseWriter, req *http.Request) {
 	totalRunRequests.Inc()
 	start := time.Now()
 	query := req.URL.Query()
+
+	iterations := 1
+	if query.Has("iterations") {
+		i, err := strconv.Atoi(query.Get("iterations"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = fmt.Fprintf(w, "Error: invalid value for iterations: %v", err)
+		}
+		iterations = i
+	}
+
 	switch function := query.Get("function"); function {
 	case "euler":
-		euler(500_000)
+		euler(iterations)
+	case "keygen":
+		err := keygen(iterations)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = fmt.Fprintf(w, "Error: error in key generation: %v", err)
+		}
 	case "":
 		w.WriteHeader(http.StatusBadRequest)
 		_, _ = fmt.Fprintf(w, "Error: missing function parameter in query string")
@@ -63,8 +106,9 @@ func run(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		_, _ = fmt.Fprintf(w, "Error: unknown function '%s'\n", html.EscapeString(function))
 	}
+
 	duration := time.Since(start).Seconds()
-	_, _ = fmt.Fprintf(w, "%f", duration)
+	_, _ = fmt.Fprintf(w, "\nElapsed Time: %f", duration)
 	runDuration.Set(duration)
 
 }
